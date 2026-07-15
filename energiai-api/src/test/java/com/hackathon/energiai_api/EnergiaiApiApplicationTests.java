3079b9f5d72fa026c6a65e6cc353338bc2812963
Josue Leon <josueleon.102013@gmail.com>
2026-07-15 16:05:02 -0600

    chore: agregar estructura inicial del backend Spring Boot

---
+++ energiai-api/src/test/java/com/hackathon/energiai_api/EnergiaiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.energiai_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergiaiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
